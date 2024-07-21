49f51f04ed70af69fcd74d7296280a36d7923e24
FibersTV <tsaul2430@gmail.com>
2024-07-21 19:22:26 -0400

    Actualizar chmod_protocol.sh

--- conexiones/chmod_protocol.sh
+++ conexiones/chmod_protocol.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Script para otorgar permisos de ejecución a todos los archivos de protocolo
 
-# Cambiar permisos de ejecución a todos los archivos en las subcarpetas de 'conexiones'
-find ~/FibersTV/conexiones -type f -name "*.sh" -exec chmod +x {} \;
+# Otorgar permisos de ejecución a cada archivo de protocolo
+chmod +x /root/FibersTV/conexiones/dropbear/dropbear.sh
+chmod +x /root/FibersTV/conexiones/socks_python/socks_python.sh
+chmod +x /root/FibersTV/conexiones/ssl/ssl.sh
+chmod +x /root/FibersTV/conexiones/v2ray/v2ray.sh
+chmod +x /root/FibersTV/conexiones/websocket/websocket.sh
+chmod +x /root/FibersTV/conexiones/badvpn_udp/badvpn_udp.sh
+chmod +x /root/FibersTV/conexiones/squid/squid.sh
+chmod +x /root/FibersTV/conexiones/openvpn/openvpn.sh
+chmod +x /root/FibersTV/conexiones/slowdns/slowdns.sh
+chmod +x /root/FibersTV/conexiones/wireguard/wireguard.sh
+chmod +x /root/FibersTV/conexiones/udp/udp.sh
+chmod +x /root/FibersTV/conexiones/psiphon/psiphon.sh
+chmod +x /root/FibersTV/conexiones/ws_epro/ws_epro.sh
 
 echo "Permisos de ejecución otorgados a todos los archivos de protocolo."
