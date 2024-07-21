9a5272daad5076632ecd0d0dd0481a87cdc158f7
FibersTV <tsaul2430@gmail.com>
2024-07-21 09:51:22 -0400

    Update FibersTV.sh

--- FibersTV.sh
+++ FibersTV.sh
@@ -11,57 +11,45 @@ COL_ERROR="#FF6347"
 mostrar_banner() {
     clear
     echo -e "\e[48;5;39m\e[97m**********************************************\e[0m"
-    echo -e "\e[48;5;39m\e[97m*             \e[48;5;39m\e[97m   FIBERSTV.COM   \e[0m\e[48;5;39m\e[97m*\e[0m"
+    echo -e "\e[48;5;39m\e[97m*             \e[38;5;82mFIBERSTV.COM\e[0m\e[48;5;39m\e[97m*\e[0m"
     echo -e "\e[48;5;39m\e[97m**********************************************\e[0m"
 }
 
 # Función para mostrar la información del sistema
 mostrar_info_sistema() {
-    echo -e "\e[48;5;40m\e[97mMemoria RAM: $(free -h | grep Mem | awk '{print $3 "/" $2}')\e[0m"
-    echo -e "\e[48;5;40m\e[97mCPU: $(lscpu | grep 'Model name' | awk -F: '{print $2}' | xargs)\e[0m"
-    echo -e "\e[48;5;40m\e[97mFecha y Hora: $(date)\e[0m"
+    echo -e "Memoria RAM: \e[38;5;82m$(free -h | grep Mem | awk '{print $3 "/" $2}')\e[0m"
+    echo -e "CPU: \e[38;5;82m$(lscpu | grep 'Model name' | awk -F: '{print $2}' | xargs)\e[0m"
+    echo -e "Fecha y Hora: \e[38;5;82m$(date)\e[0m"
 }
 
 # Función para mostrar el menú principal
 menu_principal() {
     mostrar_banner
     mostrar_info_sistema
-    echo -e "\e[48;5;28m\e[97mPANEL DE CONTROL\e[0m"
-    echo -e "\e[48;5;28m\e[97m1) \e[48;5;33m\e[97mUSUARIOS \e[0m"
-    echo -e "\e[48;5;28m\e[97m2) \e[48;5;33m\e[97mCONEXIONES \e[0m"
-    echo -e "\e[48;5;28m\e[97m3) \e[48;5;33m\e[97mHERRAMIENTAS \e[0m"
-    echo -e "\e[48;5;28m\e[97m4) \e[48;5;33m\e[97mBACKUP \e[0m"
-    echo -e "\e[48;5;28m\e[97m5) \e[48;5;33m\e[97mACTUALIZAR \e[0m"
-    echo -e "\e[48;5;28m\e[97m6) \e[48;5;33m\e[97mDESINSTALAR \e[0m"
-    echo -e "\e[48;5;28m\e[97m0) \e[48;5;33m\e[97mSALIR DEL SCRIPT \e[0m"
+    echo -e "\e[38;5;39mPANEL DE CONTROL\e[0m"
+    echo -e "1) \e[38;5;33mCUENTAS\e[0m"
+    echo -e "2) \e[38;5;33mCONEXIONES\e[0m"
+    echo -e "3) \e[38;5;33mHERRAMIENTAS\e[0m"
+    echo -e "4) \e[38;5;33mBACKUP\e[0m"
+    echo -e "5) \e[38;5;33mACTUALIZAR\e[0m"
+    echo -e "6) \e[38;5;33mDESINSTALAR\e[0m"
+    echo -e "0) \e[38;5;33mSALIR DEL SCRIPT\e[0m"
     echo -n "Seleccione una opción: "
     read opcion
 
     case $opcion in
-        1) menu_usuarios ;;
-        2) menu_conexiones ;;
-        3) menu_herramientas ;;
-        4) menu_backup ;;
-        5) actualizar ;;
-        6) desinstalar ;;
+        1) ./cuentas/cuentas.sh ;;
+        2) ./conexiones/conexiones.sh ;;
+        3) ./herramientas/herramientas.sh ;;
+        4) ./backup/backup.sh ;;
+        5) ./actualizar/actualizar.sh ;;
+        6) ./desinstalar/desinstalar.sh ;;
         0) exit ;;
-        *) echo -e "\e[48;5;9m\e[97mOpción inválida\e[0m"; sleep 2; menu_principal ;;
+        *) echo -e "\e[38;5;9mOpción inválida\e[0m"; sleep 2 ;;
     esac
 }
 
-# Función para el menú de usuarios (ejemplo de implementación)
-menu_usuarios() {
-    clear
-    echo -e "\e[48;5;33m\e[97mMenu Usuarios\e[0m"
-    # Agregar opciones y lógica para el menú de usuarios
-    echo -e "\e[48;5;33m\e[97m0) Volver al menú principal\e[0m"
-    echo -n "Seleccione una opción: "
-    read opcion
-    case $opcion in
-        0) menu_principal ;;
-        *) echo -e "\e[48;5;9m\e[97mOpción inválida\e[0m"; sleep 2; menu_usuarios ;;
-    esac
-}
-
-# Llamar al menú principal al iniciar el script
-menu_principal
+# Bucle para mantener el menú principal activo
+while true; do
+    menu_principal
+done
